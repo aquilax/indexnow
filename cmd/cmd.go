ec831b60a72b3c8b00cff9d04251946c67e19b44
aquilax <aquilax@gmail.com>
2021-12-02 16:00:10 +0100

    Useable command line tool

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
@@ -9,14 +10,30 @@ import (
 )
 
 func main() {
-	searchEngineHost := os.Args[1]
-	key := os.Args[2]
-	urlToAdd := os.Args[3]
-	in := indexnow.New(searchEngineHost, &indexnow.Ownership{Key: key}, http.DefaultTransport)
-	urlToSubmit := indexnow.GetSingleSubmitUrl(searchEngineHost, key, "", urlToAdd)
+	var searchEngineHost string
+	var key string
+	var keyLocation string
+	var dryRun bool
+	flag.StringVar(&searchEngineHost, "host", "", "Search engine hostname")
+	flag.StringVar(&key, "key", "", "Submission key")
+	flag.StringVar(&keyLocation, "keyLocation", "", "Submission key location URL")
+	flag.BoolVar(&dryRun, "dryRun", false, "Dry run")
+	flag.Parse()
+
+	urlToAdd := flag.Arg(0)
+
+	if searchEngineHost == "" || (key == "" && keyLocation == "") || urlToAdd == "" {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	urlToSubmit := indexnow.GetSingleSubmitUrl(searchEngineHost, key, keyLocation, urlToAdd)
 	fmt.Println(urlToSubmit)
-	_, err := in.SubmitSingleURL(urlToAdd)
-	if err != nil {
-		panic(err)
+	if !dryRun {
+		in := indexnow.New(searchEngineHost, &indexnow.Ownership{Key: key, KeyLocation: keyLocation}, http.DefaultTransport)
+		_, err := in.SubmitSingleURL(urlToAdd)
+		if err != nil {
+			panic(err)
+		}
 	}
 }
